94446217f8fe532ea92a8d7029823c3f3574d687
vere <vdiongzon@startechup.com>
2018-10-15 19:02:59 +0800

    adding transpose function for matrix class

--- matrix.py
+++ matrix.py
@@ -32,6 +32,26 @@ class Matrix:
                     self.matrix[i][j] += n
 
     def multiply(self, n):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.matrix[i][j] *= n
+        if isinstance(n, Matrix):
+            if self.cols != n.rows:
+                raise Exception("Columns of A must match rows of B")
+
+            result = Matrix(self.rows, n.cols)
+
+            for i in range(result.rows):
+                for j in range(result.cols):
+                    total_sum = 0
+                    for k in self.cols:
+                        total_sum += self.matrix[i][k] * n.matrix[k][j]
+                    result.matrix[i][j] = total_sum
+
+            self.matrix = result.matrix
+
+    def transpose(self):
+        result = Matrix(self.cols, self.rows)
+
+        for i in range(result.rows):
+            for j in range(result.cols):
+                result.matrix[j][i] = self.matrix[i][j]
+
+        return result
